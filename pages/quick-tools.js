a0e771e9d7fc7c9aaffffe14b2a5ebd6bacf589d
Rich Bakos <rbakos@gmail.com>
2025-08-09 15:29:46 -0600

    New: added a few quick tools

--- pages/quick-tools.js
+++ pages/quick-tools.js
@@ -37,7 +37,7 @@ export default function QuickTools() {
 
     return (
         <div className="max-w-4xl mx-auto p-6">
-            <h1 className="text-3xl font-bold mb-4">Quickies</h1>
+            <h1 className="text-3xl font-bold mb-4">Quick Tools</h1>
 
             {/* Tabs */}
             <div className="flex space-x-2 mb-6">
